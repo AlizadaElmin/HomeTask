5a3a3878bd76312bcd720a4f8b588754df89ea29
ElminAlizada <alizadaelmin30@gmail.com>
2024-10-09 01:33:04 +0400

    Arrays tasks added

---
+++ Arrays.08.10/ArrayTask1/ArrayTask1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Verilmiş 2 integer array-dən birincisində yer alıb digərində yer almayan elementləri ekrana çıxarın
+
+int[] arr1 = { 1, 3, 4, 5, 6, 8, 9};
+int[] arr2 = { 1, 4, 0, 12, 2, 7,11 };
+bool isExist = false;
+
+
+for (int i =0; i < arr1.Length; i++)
+{
+    isExist = false;
+    for(int j =0; j < arr2.Length; j++)
+    {
+        if (arr1[i] == arr2[j])
+        {
+            isExist = true;
+            break;
+        }
+    }
+
+    if (!isExist)
+    {
+        Console.WriteLine(arr1[i]);
+    }
+    
+}
+
+
+
---
+++ Arrays.08.10/ArrayTask2/ArrayTask2/Program.cs
@@ -0,0 +1,47 @@
+﻿// Verilmiş integer array-in tək indexdə duran elementləri ilə cüt indexdə duran elementləri müqayisə edin
+
+int[] nums = { 1, 2, 3, 4, 5, 6};
+int oddIndex = 0;
+int evenIndex = 0;
+
+
+
+for(int i = 0; i < nums.Length; i++)
+{
+    if(i % 2 == 0)
+    {
+        evenIndex += nums[i];
+    }
+    else
+    {
+        oddIndex += nums[i];
+    }
+}
+
+
+if (evenIndex > oddIndex)
+{
+    Console.WriteLine("Cut index cemi boyukdur");
+}
+else if(evenIndex == oddIndex)
+{
+    Console.WriteLine("Beraberdir");
+}
+else
+{
+    Console.WriteLine("Tek index cemi boyukdur");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
