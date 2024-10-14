410ce202dcfd6017d49484aff01a102731daea66
ElminAlizada <alizadaelmin30@gmail.com>
2024-10-14 22:51:30 +0400

    Class task added

---
+++ ClassFirstTask14.10/ClassFirstTask/Product.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+
+namespace ClassFirstTask;
+
+public class Product
+{
+    public string Name;
+    public string BrandName;
+    public double Price;
+
+    public Product(string name, string brandName, double price)
+    {
+        Name = name;
+        BrandName = brandName;
+        Price = price;
+    }
+}
---
+++ ClassFirstTask14.10/ClassFirstTask/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ClassFirstTask;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Product[] products =
+        {
+            new Product("15 pro", "Iphone", 1700),
+            new Product("14 pro", "Iphone", 1500),
+            new Product("13 pro", "Iphone", 1300),
+            new Product("12 pro", "Iphone", 1100),
+        };
+        CheckPrice(products,1000,2000);
+    }
+
+    static void CheckPrice(Product[] products, double minPrice, double maxPrice)
+    {
+        foreach (Product product in products)
+        {
+            if (minPrice <= product.Price && product.Price <= maxPrice)
+            {
+                Console.WriteLine($"Məhsul {product.Name} dəyəri {product.Price}");
+            }
+        }
+    }
+}
